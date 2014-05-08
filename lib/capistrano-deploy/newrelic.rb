090396a010ad340039f8a409727d246dd1f19984
Jordan Babe <jordan.babe@ama.ab.ca>
2014-05-08 15:56:06 -0600

    Only run New Relic notifications on primary prod
    * Now this task only gets run on 1 server (the primary), it will also
    * only run on the production stage (but this is configurable via setting
    :new_relic_stages

--- lib/capistrano-deploy/newrelic.rb
+++ lib/capistrano-deploy/newrelic.rb
@@ -4,17 +4,20 @@ module CapistranoDeploy
     def self.load_into(configuration)
       configuration.load do
 
+        set(:new_relic_stages) { %w(production) }
         set(:new_relic_user) { (%x(git config user.name)).chomp }
         set(:current_revision) { capture("cd #{deploy_to} && git rev-parse HEAD").chomp }
         set(:link) { "https://api.newrelic.com/deployments.xml" }
 
         namespace :newrelic do
 
-          task :notice_deployment, roles: :notification do
+          task :notice_deployment, roles: :notification, only: { primary: true } do
+            if new_relic_stages.include? current_stage
             run "curl -sH '#{new_relic_api_key}'
                 -d 'deployment[app_name]=#{new_relic_app_name}'
                 -d 'deployment[revision]=#{current_revision}'
                 -d 'deployment[user]=#{new_relic_user}' #{link}"
+            end
           end
         end
 
